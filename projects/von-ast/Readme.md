0897277869545046d63ab8ed04a0eced1927e851
Aster <galaster@foxmail.com>
2022-10-29 19:40:21 +0800

    Support custom node

---
+++ projects/von-ast/Readme.md
@@ -0,0 +1,14 @@
+Virtual Object Notation
+=======================
+
+
+| Type | Value | A |
+| :--: | :--: | :---: |
+| Number | `2.0`| 1 |
+| String | `s` | 1 |
+
+```von 
+[
+    a: 2
+]
+```
--- projects/von-ast/src/ast/mod.rs
+++ projects/von-ast/src/ast/mod.rs
@@ -1,119 +1,30 @@
-use std::{
-    cmp::Ordering,
-    collections::BTreeMap,
-    fmt::{Debug, Formatter},
-};
-
 use bigdecimal::BigDecimal;
+use diagnostic::Span;
 use indexmap::IndexMap;
-use vos_error::Span;
+use num::BigInt;
 
-mod constraint;
-mod define;
 mod display;
-mod table;
-mod value;
-
-#[derive(Clone, Default)]
-pub struct VosAST {
-    pub statements: Vec<VosStatement>,
-}
+mod ser;
+mod der;
 
 #[derive(Clone)]
-pub enum VosStatement {
-    Table(Box<TableStatement>),
-    Object(Box<ObjectStatement>),
-    Union(Box<UnionStatement>),
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct TableStatement {
-    pub kind: TableKind,
-    pub name: Identifier,
-    pub fields: IndexMap<String, FieldStatement>,
-    pub constraints: BTreeMap<String, ConstraintStatement>,
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct ObjectStatement {
-    pub name: Identifier,
-    pub value: ValueStatement,
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct UnionStatement {
-    pub name: Identifier,
-    pub value: ValueStatement,
-}
-
-#[derive(Debug, Clone)]
-pub enum TableKind {
-    /// Compact structures, lower size and better performance, any changes will break compatibility
-    ///
-    /// - ❌ change order
-    /// - ❌ add fields
-    /// - ❌ change types
-    /// - ❌ delete fields
-    Structure,
-    /// Structure with vtable, any changes will break compatibility
-    ///
-    /// - ✔️ change order
-    /// - ✔️ add fields
-    /// - ❌ change types
-    /// - ❌ delete fields
-    Table,
-}
-
-#[derive(Clone, Default)]
-pub struct FieldStatement {
-    pub name: Identifier,
-    pub typing: FieldTyping,
-    pub value: ValueStatement,
-}
-
-#[derive(Clone, Default)]
-pub struct FieldTyping {
-    pub namespace: Namespace,
-    pub generics: GenericStatement,
-}
-
-#[derive(Clone, PartialEq)]
-pub enum GenericStatement {
-    Nothing,
-    NumberBound { symbol: Ordering, number: BigDecimal, inclusive: bool },
-    NumberRange { min: BigDecimal, min_inclusive: bool, max: BigDecimal, max_inclusive: bool },
-    Arguments { arguments: Vec<ValueStatement> },
-}
-
-#[derive(Debug, Clone)]
-pub struct ConstraintStatement {
-    pub name: Identifier,
-    pub value: ValueStatement,
-}
-
-#[derive(Clone, Default)]
-pub struct ValueStatement {
-    pub kind: ValueKind,
+pub struct VonNode {
+    pub kind: VonKind,
+    pub hint: Identifier,
     pub span: Span,
 }
 
-#[derive(Clone, PartialEq)]
-pub enum ValueKind {
-    Null,
+#[derive(Clone)]
+pub enum VonKind {
     Boolean(bool),
-    String(String),
-    Number(BigDecimal),
-    Symbol(Namespace),
-    List(Vec<ValueStatement>),
-    Dict(IndexMap<String, ValueStatement>),
+    Integer(BigInt),
+    Decimal(BigDecimal),
+    Text(String),
+    List(Vec<VonNode>),
+    Dict(IndexMap<String, VonNode>),
 }
 
-#[derive(Clone, Default, PartialEq)]
-pub struct Namespace {
-    pub scope: Vec<Identifier>,
-}
-
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct Identifier {
     pub id: String,
     pub span: Span,
